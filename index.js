c3b3383805350b1a5ed4e394dafc2a3222b884ff
khaled swidan <khaledqutb4@gmail.com>
2023-07-03 02:37:59 +0300

    change api source

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const weatherDetails = document.querySelector(".weather-details");
 const error404 = document.querySelector(".not-found");
 
 search.addEventListener("click", () => {
-  const APIKey = "bdbf12ffb35e452681e170138233006";
+  const APIKey = "07ab8bbd34797a34e35b3fd1d99b493e";
   const city = document.querySelector(".search-box input").value;
 
   if (city === "") return;
 
   fetch(
-    `http://api.weatherapi.com/v1/current.json?key=${APIKey}&q=${city}&aqi=no`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
   )
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
 
-      if (data.error) {
+      if (data.cod === "404") {
         const error = document.querySelector(".error");
         container.style.height = "fit-content";
         weatherBox.style.display = "none";
         weatherDetails.style.display = "none";
         error404.style.display = "block";
         error404.classList.add("fadeIn");
-        error.innerHTML = data.error.message;
+        error.innerHTML = data.message;
         return;
       }
 
@@ -39,15 +39,21 @@ search.addEventListener("click", () => {
       const lastUpdate = document.querySelector(".lastUpdate");
       const humidity = document.querySelector(".humidity .text span");
       const wind = document.querySelector(".wind .text span");
+      let options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      };
 
-      country.innerHTML = data.location.country;
-      region.innerHTML = data.location.region;
-      temperature.innerHTML = `${data.current.temp_c}<span>°C</span>`;
-      icon.setAttribute("src", `https:${data.current.condition.icon}`);
-      text.innerHTML = data.current.condition.text;
-      lastUpdate.innerHTML = data.current.last_updated;
-      humidity.innerHTML = `${data.current.humidity}`;
-      wind.innerHTML = `${data.current.wind_kph}<span>kph</span>`;
+      country.innerHTML = data.name;
+      region.innerHTML = data.sys.country;
+      temperature.innerHTML = `${data.main.temp}<span>°C</span>`;
+      icon.setAttribute("src", `https:${data.weather[0].icon}`);
+      text.innerHTML = data.weather[0].description;
+      lastUpdate.innerHTML = new Date().toLocaleDateString("en-US", options);
+      humidity.innerHTML = `${data.main.humidity}`;
+      wind.innerHTML = `${data.wind.speed}<span>kph</span>`;
 
       weatherBox.style.display = "";
       weatherDetails.style.display = "";
